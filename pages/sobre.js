41feecd60d4fdae4d2481c2cd4f397ffb226e88d
James F. Arrais <87504708+jamesfagno@users.noreply.github.com>
2023-03-09 13:23:37 -0300

    ...

--- pages/sobre.js
+++ pages/sobre.js
@@ -9,18 +9,5 @@ function sobre (){
         </div>
     )
 }
-function Contador(){
-    const [Contador, setContador] = useState(1);
-
-    function adicionarContador(){
-        setContador (Contador + 1);
-    }
-    return(
-        <div>
-            <div>{Contador}</div>
-            <button onClick={adicionarContador} >Adicionar</button>
-        </div>
-    )
-}
 
 export default sobre
